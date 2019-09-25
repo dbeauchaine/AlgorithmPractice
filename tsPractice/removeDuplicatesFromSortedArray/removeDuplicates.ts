d71e75f53fb1485628ba0f66760dc4e4896365af
Dan Beauchaine <danbeauchaine@gmail.com>
2019-09-24 21:05:48 -0700

    wrote and tested a function to remove duplicate numbers in a sorted array of numbers.

--- tsPractice/removeDuplicatesFromSortedArray/removeDuplicates.ts
+++ tsPractice/removeDuplicatesFromSortedArray/removeDuplicates.ts
@@ -1,1 +1,16 @@
-﻿
+﻿export function removeDuplicates(array: number[]) {
+
+    let temp = array[0];
+    let i = 1;
+
+    while (i < array.length) {
+        if (array[i] === temp) {
+            array.splice(i, 1);
+        } else {
+            temp = array[i];
+            i++;
+        }
+    };
+
+    return array.length;
+}
