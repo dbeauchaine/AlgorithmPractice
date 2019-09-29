c6f78877e461d5a443c755901b550a8c70f2a17b
Dan Beauchaine <danbeauchaine@gmail.com>
2019-09-28 20:50:32 -0700

    Further implemented BuyAndSellStocks
    Basic implementation, needs refactoring and additional testing for arrays of numbers that rise and fall multiple times.

--- tsPractice/BuyAndSellStock/BuyAndSellStock.ts
+++ tsPractice/BuyAndSellStock/BuyAndSellStock.ts
@@ -1,3 +1,27 @@
 ﻿export function buyAndSellStocks(array: number[]) {
-    return 0;
+
+    let profit = 0;
+    let purchased: number = null;
+
+    if (array.length >= 2) {
+        for (let i = 1; i < array.length; i++) {
+            if (array[i - 1] < array[i]) {
+                if (!purchased) {
+                    purchased = array[i - 1];
+                }
+
+            } else if(array[i-1] > array[i]) {
+                if (purchased) {
+                    profit += array[i - 1] - purchased;
+                    purchased = null;
+                } else {
+                    purchased = array[i];
+                }
+            }
+            if (i + 1 === array.length && purchased) {
+                profit += array[i] - purchased;
+            }
+        }
+    }
+    return profit;
 }
--- tsPractice/BuyAndSellStock/BuyAndSellStocks.test.ts
+++ tsPractice/BuyAndSellStock/BuyAndSellStocks.test.ts
@@ -1,14 +1,20 @@
 ﻿import { buyAndSellStocks } from "./BuyAndSellStock";
 
 describe('BuyAndSellStocksTests', () => {
-    let testCases: TestCase[] = [{ profit: 0, inputArray: [1], description: "Array length of 1 returns 0" }]
+    let testCases: TestCase[] = [
+        { profit: 0, inputArray: [1], description: "Array length of 1 returns 0" },
+        { profit: 1, inputArray: [1, 2], description: "Buy on day 1, sell on day 2. Profit = 1." },
+        { profit: 2, inputArray: [1, 2, 3], description: "Buy on day 1, sell on day 3. Profit = 2. Array length 3." },
+        { profit: 2, inputArray: [1, 2, 3, 1], description: "Buy on day 1, sell on day 3. Profit = 2. Array length 4." },
+        { profit: 0, inputArray: [7,6,4,3,1], description: "Never buy, Profit = 0. Array length 5." }
+    ]
 
     testCases.forEach((scenario: TestCase) => {
 
         it(`Returns the total profit from buying and selling stocks: ${scenario.description}`, function () {
             let profit = buyAndSellStocks(scenario.inputArray);
 
-            expect(profit === scenario.profit);
+            expect(profit).toEqual(scenario.profit);
         });
     });
 
