ce2ebd965d9d0382d9898d6abf018be018b40853
Dan Beauchaine <danbeauchaine@gmail.com>
2019-09-28 19:38:51 -0700

    Started Implementing buyAndSellStocks function and tests
    Began with most basic implementation of function.

--- tsPractice/BuyAndSellStock/BuyAndSellStocks.test.ts
+++ tsPractice/BuyAndSellStock/BuyAndSellStocks.test.ts
@@ -1,6 +1,20 @@
-﻿describe('Test Suite 1', () => {
-    it("BuyAndSellStocksTests", function () {
-        expect(true).toBeTruthy();
+﻿import { buyAndSellStocks } from "./BuyAndSellStock";
+
+describe('BuyAndSellStocksTests', () => {
+    let testCases: TestCase[] = [{ profit: 0, inputArray: [1], description: "Array length of 1 returns 0" }]
+
+    testCases.forEach((scenario: TestCase) => {
+
+        it(`Returns the total profit from buying and selling stocks: ${scenario.description}`, function () {
+            let profit = buyAndSellStocks(scenario.inputArray);
+
+            expect(profit === scenario.profit);
+        });
     });
 
+    interface TestCase {
+        profit: number;
+        inputArray: number[];
+        description: string;
+    }
 });
