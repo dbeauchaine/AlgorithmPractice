0b12cc047eafb5fca6f7fa220594154f483fd481
Dan Beauchaine <danbeauchaine@gmail.com>
2019-09-28 21:02:10 -0700

    Further implementation of buyAndSellStocks
    Tested arrays that increased and decreased multiple times. Tested arrays with duplicate entries. Tested an array entirely composed of duplicates. Requires refactoring.

--- tsPractice/BuyAndSellStock/BuyAndSellStocks.test.ts
+++ tsPractice/BuyAndSellStock/BuyAndSellStocks.test.ts
@@ -6,7 +6,11 @@ describe('BuyAndSellStocksTests', () => {
         { profit: 1, inputArray: [1, 2], description: "Buy on day 1, sell on day 2. Profit = 1." },
         { profit: 2, inputArray: [1, 2, 3], description: "Buy on day 1, sell on day 3. Profit = 2. Array length 3." },
         { profit: 2, inputArray: [1, 2, 3, 1], description: "Buy on day 1, sell on day 3. Profit = 2. Array length 4." },
-        { profit: 0, inputArray: [7,6,4,3,1], description: "Never buy, Profit = 0. Array length 5." }
+        { profit: 0, inputArray: [7, 6, 4, 3, 1], description: "Never buy, Profit = 0. Array length 5." },
+        { profit: 7, inputArray: [7, 1, 5, 3, 6, 4], description: "Buy on day 2, sell on day 3, buy on day 4, sell on day 5. Profit = 7. Array length 6." },
+        { profit: 2, inputArray: [1, 2, 2, 3], description: "Buy on day one, sell on day 4. Profit = 2. Duplicate number." },
+        { profit: 2, inputArray: [1, 2, 3, 3], description: "Buy on day one, sell on day 4. Profit = 2. Duplicate on selling day." },
+        { profit: 0, inputArray: [1, 1, 1, 1, 1, 1], description: "Never buy, profit = 0. All duplicates." }
     ]
 
     testCases.forEach((scenario: TestCase) => {
