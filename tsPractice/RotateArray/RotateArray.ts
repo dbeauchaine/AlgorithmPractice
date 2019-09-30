c19ece258e95ad21ef5d8bfdcb3e432a62c76144
Dan Beauchaine <danbeauchaine@gmail.com>
2019-09-30 13:10:55 -0700

    Initial commit for exercise rotateArray
    Created initial tests and function rotateArray. Requires additional testing, and basic implementation of function.

---
+++ tsPractice/RotateArray/RotateArray.ts
@@ -0,0 +1,7 @@
+﻿export function rotateArray(arrayToRotate: number[], k:number) {
+    if (k > 0) {
+        let temp = arrayToRotate[0];
+        arrayToRotate[0] = arrayToRotate[1];
+        arrayToRotate[1] = temp;
+    }
+}
---
+++ tsPractice/RotateArray/RotateArrayTest.test.ts
@@ -0,0 +1,34 @@
+﻿import { rotateArray } from "./RotateArray";
+
+describe('rotateArrayTests', ()=> {
+    let testCases: TestCase[] = [
+        { arrayToRotate: [1], arraySolution: [1], k: 0, description: "No rotation." },
+        { arrayToRotate: [1, 2], arraySolution: [2, 1], k: 1, description: "rotate 1 with 2 elements, essentially a swap."}
+        ];
+    testCases.forEach((testCases: TestCase) => {
+        it(`Returns void, rotates the passed array. ${testCases.description}`, function () {
+            rotateArray(testCases.arrayToRotate, testCases.k);
+            expect(verifyArray(testCases.arraySolution, testCases.arrayToRotate)).toBeTruthy();
+        });
+
+    })
+
+    interface TestCase {
+        arrayToRotate: number[];
+        arraySolution: number[];
+        k: number;
+        description: string;
+    }
+
+    function verifyArray(modifiedArray: number[], expectedArray: number[]) {
+        let areEqual = true;
+
+        for (let i = 0; i < expectedArray.length; i++) {
+            if (expectedArray[i] !== modifiedArray[i]) {
+                areEqual = false;
+            }
+        }
+
+        return areEqual;
+    }
+});
