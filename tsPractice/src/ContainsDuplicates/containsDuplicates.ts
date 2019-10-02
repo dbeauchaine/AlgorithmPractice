3ec067f5ff5ca7b0401ee585ef327412e4181fa3
Dan Beauchaine <danbeauchaine@gmail.com>
2019-10-01 21:04:35 -0700

    Initial implementation of containsDuplicate.
    Created tests and initial implementation of function to return true when there is a duplicate and false if there is not in a number array.

---
+++ tsPractice/src/ContainsDuplicates/containsDuplicates.ts
@@ -0,0 +1,13 @@
+﻿export function containsDuplicate(inputArray: number[]) {
+    const numberSet = new Set();
+
+    for (let i = 0; i < inputArray.length; i++) {
+        if (numberSet.has(inputArray[i])) {
+            return true;
+        } else {
+            numberSet.add(inputArray[i]);
+        }
+    }
+
+    return false;
+}
