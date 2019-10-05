82777a29965a2c33ce77131965282b585a4ac3bb
Dan Beauchaine <danbeauchaine@gmail.com>
2019-10-05 15:41:35 -0700

    Further implementation of the SingleNumber function
    Added tests, added a map data structure.

--- tsPractice/src/SingleNumber/singleNumber.ts
+++ tsPractice/src/SingleNumber/singleNumber.ts
@@ -1,3 +1,22 @@
 export function singleNumber(inputArray: number[]) {
-    return 1;
+    const numberMap: Map<number, number> = new Map();
+
+    for (let i = 0; i < inputArray.length; i++) {
+
+        let count = numberMap.get(inputArray[i]);
+        if (count) {
+            count++;
+            numberMap.set(inputArray[i], count);
+        } else {
+            numberMap.set(inputArray[i], 1);
+        }
+    }
+
+    for (const [key, value] of numberMap.entries()) {
+        if (value === 1) {
+            return key;
+        }
+    }
+
+    return null;
 }
