604b28f62922440d8980e53604293a1608b958c1
dbeauchaine <danbeauchaine@gmail.com>
2019-10-08 09:39:25 -0700

    Further implementation of MoveZeroes
    Added additional test cases. Refactored variable names for readability.

--- tsPractice/src/MoveZeroes/moveZeroes.test.ts
+++ tsPractice/src/MoveZeroes/moveZeroes.test.ts
@@ -3,10 +3,12 @@ import { moveZeroes } from "./moveZeroes";
 describe("moveZeroesTests", () => {
     const testCases: TestCase[] = [
         { inputArray: [], expectedOutput: [], description: "Array length zero, no zeroes." },
-        { inputArray: [0], expectedOutput: [0], description: "Array length one with one zero." },
+        { inputArray: [0], expectedOutput: [0], description: "Array length one with one zero. Array with only Zeroes." },
         { inputArray: [1, 0], expectedOutput: [1, 0], description: "Array length 2 with one zero. No change required." },
         { inputArray: [0, 1], expectedOutput: [1, 0], description: "Array length 2, one zero." },
-        { inputArray: [0, 0, 1], expectedOutput: [1, 0, 0], description: "Array length 3, 2 zeroes." }
+        { inputArray: [0, 0, 1], expectedOutput: [1, 0, 0], description: "Array length 3, 2 zeroes." },
+        { inputArray: [1, 0, 0, 2, 0], expectedOutput: [1, 2, 0, 0, 0], description: "array length 5, 3 zeroes." },
+        { inputArray: [1, 1, 2, 3], expectedOutput: [1, 1, 2, 3], description: "Array length 4. No zeroes." }
     ];
 
     testCases.forEach((testCase: TestCase) => {
--- tsPractice/src/MoveZeroes/moveZeroes.ts
+++ tsPractice/src/MoveZeroes/moveZeroes.ts
@@ -1,11 +1,11 @@
 export function moveZeroes(inputArray: number[]): number[] {
-    let length = inputArray.length;
-    for (let i = 0; i < length; i++) {
-        if (inputArray[i] === 0) {
-            inputArray.splice(i, 1);
+    let lastUncheckedElement = inputArray.length;
+    for (let lastCheckedElement = 0; lastCheckedElement < lastUncheckedElement; lastCheckedElement++) {
+        if (inputArray[lastCheckedElement] === 0) {
+            inputArray.splice(lastCheckedElement, 1);
             inputArray.push(0);
-            length--;
-            i--;
+            lastUncheckedElement--;
+            lastCheckedElement--;
         }
     }
 
