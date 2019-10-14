c041053b8aa1d82018ed3b22e96428989c1b09de
Dan Beauchaine <danbeauchaine@gmail.com>
2019-10-13 17:36:59 -0700

    Final Implementation of rotateImage
    Refactored variable names and isolated logic in individual functions.

--- tsPractice/src/RotateImage/rotateImage.ts
+++ tsPractice/src/RotateImage/rotateImage.ts
@@ -1,27 +1,36 @@
 export function rotateImage(matrix: number[][]): void {
-    const lastIndex = matrix.length - 1;
-    let index = lastIndex;
+    let lastIndex = calculateLastIndex(matrix);
 
     for (let row = 0; row < lastIndex; row++) {
-        for (let column = row; column < index; column++) {
-            let currentRow = row;
-            let currentColumn = column;
-            const temp = matrix[row][column];
-
-            do {
-                const nextRow = lastIndex - currentColumn;
-                const nextColumn = currentRow;
-
-                if (nextRow === row && nextColumn === column) {
-                    matrix[currentRow][currentColumn] = temp;
-                } else {
-                    matrix[currentRow][currentColumn] = matrix[nextRow][nextColumn];
-                }
-
-                currentRow = nextRow;
-                currentColumn = nextColumn;
-            } while (!(currentRow === row && currentColumn === column));
+        for (let column = row; column < lastIndex; column++) {
+            rotateFourElements(matrix, row, column);
         }
-        index--;
+
+        lastIndex--;
     }
+}
+
+function rotateFourElements(matrix: number[][], row: number, column: number): void {
+    const lastIndex = calculateLastIndex(matrix);
+    let currentRow = row;
+    let currentColumn = column;
+    const temp = matrix[row][column];
+
+    do {
+        const nextRow = lastIndex - currentColumn;
+        const nextColumn = currentRow;
+
+        if (nextRow === row && nextColumn === column) {
+            matrix[currentRow][currentColumn] = temp;
+        } else {
+            matrix[currentRow][currentColumn] = matrix[nextRow][nextColumn];
+        }
+
+        currentRow = nextRow;
+        currentColumn = nextColumn;
+    } while (!(currentRow === row && currentColumn === column));
+}
+
+function calculateLastIndex(matrix: number[][]): number {
+    return matrix.length - 1;
 }
